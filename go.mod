36cb6c13edeb62ffabfad67d4e3bd7dab1324761
Andy Doyle <andy.doyle@outlook.ie>
2025-08-31 18:31:18 +0100

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andywithcamera/git-go
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, Skippy here. Try not to screw this up.")
+}
